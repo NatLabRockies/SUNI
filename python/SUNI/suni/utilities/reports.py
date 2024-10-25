2681be6a1a325ab5af57d60e798db0c49d2aca68
ppinchuk <ppinchuk@nrel.gov>
2024-10-25 13:06:51 -0600

    Remove extra newline

--- python/SUNI/suni/utilities/reports.py
+++ python/SUNI/suni/utilities/reports.py
@@ -179,7 +179,7 @@ def compile_standard_report(results, cfg, proc_start_time):
         "DHI": float(cfg["DHIradUncert"]),
     }
 
-    sn_l = max(map(len, s_ns.values())) + 1
+    sn_l = max(map(len, s_ns.values()))
     cl_l = 4 if any(v >= 10 for v in class_uncerts.values()) else 3
     cal_l = 4 if any(v >= 10 for v in cal_uncerts.values()) else 3
     rad_l = 4 if any(v >= 10 for v in rad_uncerts.values()) else 3
@@ -202,7 +202,7 @@ def compile_standard_report(results, cfg, proc_start_time):
                 f"Cal Uncert: +/-{cal_uncertainty:>{cal_l}.1f}% ",
                 f"Cal Date: {cal_due} ",
                 f"Due Date: {due} ",
-                f"Radiometer Uncert: +/-{rad_uncertainty:>{rad_l}.1f}% ",
+                f"Radiometer Uncert: +/-{rad_uncertainty:>{rad_l}.1f}%",
             ]
         )
         lines.append(line)
